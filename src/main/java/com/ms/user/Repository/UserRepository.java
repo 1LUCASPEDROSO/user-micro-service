c30ca88bf8edc3c08b1cbb7133ead8fc5dee6ae6
lucas pedroso <pedrosolucas1745@gmail.com>
2024-08-23 17:20:36 -0300

    add: add user entity with getters and setters and the repository

---
+++ src/main/java/com/ms/user/Repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.ms.user.Repository;
+
+import com.ms.user.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
