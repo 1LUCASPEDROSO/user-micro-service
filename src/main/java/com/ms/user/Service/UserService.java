c6e3458dca371de2d9e84929265903193bc9e59c
lucas pedroso <pedrosolucas1745@gmail.com>
2024-08-25 02:13:56 -0300

    refactor: refactor user service to recive dto user, create the object and send message to RabbitMQ

--- src/main/java/com/ms/user/Service/UserService.java
+++ src/main/java/com/ms/user/Service/UserService.java
@@ -2,6 +2,7 @@ package com.ms.user.Service;
 
 import com.ms.user.DTO.UserDTO;
 import com.ms.user.Entity.User;
+import com.ms.user.Producers.UserProducer;
 import com.ms.user.Repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,15 +11,19 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final UserProducer userProducer;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, UserProducer userProducer) {
         this.userRepository = userRepository;
+        this.userProducer = userProducer;
     }
-    @Transactional
+    @Transactional // garante um rollback caso de algum erro em alguma etapa
     public User create(UserDTO dto){
         User user = new User();
         user.setName(dto.name());
         user.setEmail(dto.email());
-        return userRepository.save(user);
+        User userEntity = userRepository.save(user);
+        userProducer.publishMessageEmail(userEntity);
+        return userEntity;
     }
 }
